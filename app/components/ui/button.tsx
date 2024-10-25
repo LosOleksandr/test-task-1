bc4b732792278792de18d6de332c3c42025d6ed2
LosOleksandr <losalexander2003@gmail.com>
2024-10-25 19:37:30 +0300

    feat: add button component

---
+++ app/components/ui/button.tsx
@@ -0,0 +1,58 @@
+import * as React from "react";
+import { Slot } from "@radix-ui/react-slot";
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  asChild?: boolean;
+  variant?: "default" | "ghost";
+  size?: "icon" | "small" | "medium" | "large";
+}
+
+const getButtonVariant = (
+  variant: ButtonProps["variant"] = "default",
+  size: ButtonProps["size"] = "medium"
+) => {
+  const sizeClasses = {
+    icon: "w-10 h-10 p-1",
+    small: "py-2 text-xs",
+    medium: "py-[11px] text-sm",
+    large: "py-4 text-base",
+  };
+
+  const variantClasses = {
+    default: "text-primary_foreground bg-accent hover:bg-accent_hover",
+    ghost: "text-primary bg-transparent",
+  };
+
+  return `${variantClasses[variant]} ${sizeClasses[size]}`;
+};
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  (
+    {
+      className,
+      asChild = false,
+      variant = "default",
+      size = "medium",
+      ...props
+    },
+    ref
+  ) => {
+    const Comp = asChild ? Slot : "button";
+
+    getButtonVariant(variant);
+    return (
+      <Comp
+        className={` inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none w-full py-[11px] focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 ${getButtonVariant(
+          variant,
+          size
+        )} ${className || ""}`}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+Button.displayName = "Button";
+
+export default Button;
