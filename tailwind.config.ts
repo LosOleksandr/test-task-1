3b3f1c353a631159c59b91c788d300393b7a4292
LosOleksandr <losalexander2003@gmail.com>
2024-10-25 18:19:53 +0300

    feat: add color varuables and font for a project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,10 +10,23 @@ const config: Config = {
     extend: {
       colors: {
         background: "var(--background)",
+        background_secondary: "var(--background-secondary)",
         foreground: "var(--foreground)",
+        primary: "var(--primary)",
+        primary_foreground: "var(--primary-foreground)",
+        secondary: "var(--secondary)",
+        thirdly: "var(--thirdly)",
+        accent: "var(--accent)",
+        success: "var(--success)",
+        success_secondary: "var(--success-secondary)",
+        input: "var(--input)",
+        muted: "var(--muted)",
+        gmail: "var(--gmail)",
+        dark: "var(--dark)",
       },
     },
   },
   plugins: [],
 };
+
 export default config;
